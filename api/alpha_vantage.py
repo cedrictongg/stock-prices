09d26ba1ede7d167f63266e1801e41e565cdbdf9
Cedric Tong <ctong@calstatela.edu>
2018-05-31 19:18:06 -0700

    error checking for meta data on api request

--- api/alpha_vantage.py
+++ api/alpha_vantage.py
@@ -16,38 +16,42 @@ def daily_intraday_stock(symbol):
 
 def open_format_intraday(stock_data):
     """Formatting the output of open data"""
-    symbol = stock_data['Meta Data']['2. Symbol']
-    daily = stock_data['Time Series (1min)']
-    date = list(stock_data['Time Series (1min)'].keys())[0]
-    stock_open = '{:.2f}'.format(float(daily[date]['1. open']))
-    stock_high = '{:.2f}'.format(float(daily[date]['2. high']))
-    stock_low = '{:.2f}'.format(float(daily[date]['3. low']))
-    output = f'{symbol} is currently at ${stock_open} with a high of \
+    if 'Meta Data' in stock_data:
+        symbol = stock_data['Meta Data']['2. Symbol']
+        daily = stock_data['Time Series (1min)']
+        date = list(stock_data['Time Series (1min)'].keys())[0]
+        stock_open = '{:.2f}'.format(float(daily[date]['1. open']))
+        stock_high = '{:.2f}'.format(float(daily[date]['2. high']))
+        stock_low = '{:.2f}'.format(float(daily[date]['3. low']))
+        output = f'{symbol} is currently at ${stock_open} with a high of \
 ${stock_high} and a low of ${stock_low} today.'
-    return output
+        return output
+    return 'I could not find any stock information about that company.'
 
 
 def daily_single_stock(symbol):
     """Grab stock data"""
     function = 'TIME_SERIES_DAILY'
     url = f'{config.url}function={function}&symbol={symbol}&outputsize=compact'
-    print(url)
     req = requests.get(f'{url}&apikey={config.api_key}')
+    print(url)
     return json.loads(req.text)
 
 
 def closed_format_singles(stock_data, stock_date):
     """Formatting the output of closed data"""
     print(stock_data)
-    symbol = stock_data['Meta Data']['2. Symbol']
-    daily = stock_data['Time Series (Daily)']
-    stock_open = '{:.2f}'.format(float(daily[stock_date]['1. open']))
-    stock_high = '{:.2f}'.format(float(daily[stock_date]['2. high']))
-    stock_low = '{:.2f}'.format(float(daily[stock_date]['3. low']))
-    stock_close = '{:.2f}'.format(float(daily[stock_date]['4. close']))
-    output = f'{symbol} opened at ${stock_open} and closed at ${stock_close} with a high of \
-${stock_high} and a low of ${stock_low} as of {stock_date}'
-    return output
+    if 'Meta Data' in stock_data:
+        symbol = stock_data['Meta Data']['2. Symbol']
+        daily = stock_data['Time Series (Daily)']
+        stock_open = '{:.2f}'.format(float(daily[stock_date]['1. open']))
+        stock_high = '{:.2f}'.format(float(daily[stock_date]['2. high']))
+        stock_low = '{:.2f}'.format(float(daily[stock_date]['3. low']))
+        stock_close = '{:.2f}'.format(float(daily[stock_date]['4. close']))
+        output = f'{symbol} opened at ${stock_open} and closed at ${stock_close} with a high of \
+${stock_high} and a low of ${stock_low} as of {stock_date}.'
+        return output
+    return 'I could not find any stock information about that company.'
 
 
 def many_stocks(symbols):
